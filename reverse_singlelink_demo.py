71d5b2fb527cfa4d1f858b71874a303e58bf634e
Yang Cun <yangcun@live.com>
2016-09-17 20:31:24 +0800

    Remove the return value from the reverse_singlelink method

--- reverse_singlelink_demo.py
+++ reverse_singlelink_demo.py
@@ -13,20 +13,18 @@ def reverse_singlelink(head):
 	next = head.next
 
 	while not next == None:
+		# Hold the next's next node
 		temp = next.next
-		# cur -> next -> thirdNode
-		next.next = cur
-		# cur <- next   temp:thirdNode
+		
+		# Before:       cur -> next -> thirdNode
+		next.next = cur # Change the pointer:  a->b => a<-b
+		# After change: cur <- next    temp:thirdNode
+		
+		# Next Step, go to next two nodes
 		cur = next
-
 		next = temp
 		
-
 	head.next = None
-	head = cur
-
-	return head
-
 
 
 n1 = Node(1)
@@ -40,16 +38,16 @@ n2.next = n3
 n3.next = n4
 n4.next = n5
 
+# 1 -> 2 -> 3 -> 4 -> 5
 print(n1.val)
 print(n1.next.val)
 print(n1.next.next.val)
 print(n1.next.next.next.val)
 print(n1.next.next.next.next.val)
 
-
 reverse_singlelink(n1)
 
-
+# 1 <- 2 <- 3 <- 4 <- 5
 print(n5.val)
 print(n5.next.val)
 print(n5.next.next.val)
